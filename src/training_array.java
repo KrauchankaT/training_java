d6ff8221e3b9b25f4e4d246183000e20adb2b924
KrauchankaT <TatusyaKravchenko@gmail.com>
2026-05-29 15:38:42 +0300

    Update3

--- src/training_array.java
+++ src/training_array.java
@@ -1,6 +1,13 @@
 public class training_array {
     static void main() {
-        int a = 100;
+        // int a = 100;
         int[] array1 = {1, 34, 75, 9};
+
+        int sum = 0;
+        for (int i = 0; i < array1.length; i++) {
+            sum = sum + array1[i];
+        }
+
+        System.out.println(sum);
     }
 }
