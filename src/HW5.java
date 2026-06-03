479928e6d7a3ba26d4f3e1cb1e50f0af98d700b6
KrauchankaT <TatusyaKravchenko@gmail.com>
2026-06-03 11:20:31 +0300

    Add HW5

--- src/HW5.java
+++ src/HW5.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class HW5 {
     static void main() {
         /* Task1. Дан массив:
@@ -9,17 +11,18 @@ public class HW5 {
         System.out.println("Task 1");
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 != 0) {
-                System.out.println(array[i]);
+                System.out.print(array[i] + " ");
             }
         }
         /* Task 2.   Дан массив:
         int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
         необходимо вывести все значения массива больше 5. */
-
+        System.out.println ();
         System.out.println("Task 2");
+
         for (int i = 0; i < array.length; i++) {
             if (array[i] > 5){
-                System.out.println(array[i]);
+                System.out.print(array[i] + " ");
             }
         }
 
@@ -27,10 +30,18 @@ public class HW5 {
         int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
         необходимо увеличить все значения массива на 15. */
 
+        System.out.println();
         System.out.println("Task 3");
         for (int i = 0; i < array.length; i++) {
             int newValue = array[i];
-            System.out.println(newValue + 15);
+            System.out.print(newValue + 15);
         }
+
+        System.out.println();
+        System.out.println(Arrays.toString(array));
+        for (int i = 0; i < array.length; i++) {
+            array[i] += 15;
+        }
+        System.out.println(Arrays.toString(array));
     }
 }
