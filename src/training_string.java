568b54bfa9959259d8b3207ab03b551dae982a43
KrauchankaT <TatusyaKravchenko@gmail.com>
2026-06-05 11:02:12 +0300

    Add HW6. Working with String

--- src/training_string.java
+++ src/training_string.java
@@ -1,6 +1,15 @@
 public class training_string {
     static void main() {
         String test = "abs";
+        String test1 = "abs";
+
+        System.out.println(test == test1);
+        System.out.println(test.equals(test1));
+
+        String test2 = new String("abs");
+        System.out.println(test == test2);
+        System.out.println(test.equals(test2));
+
         System.out.println(test);
     }
 }
